5f7f29b2f6fc0957d9bd5939e4eb09e833cd00ba
Arthur Bricq <abricq@yahoo.fr>
2024-09-10 22:43:02 +0200

    Improve async mining

--- src/main_worker.rs
+++ src/main_worker.rs
@@ -67,7 +67,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let new_client = client.clone();
     let tx = tx_local_block.clone();
-    request_new_transaction_and_work(tx, new_client, cloned_token).await;
+    
+    tokio::spawn(async move {
+        request_new_transaction_and_work(tx, new_client, cloned_token).await;
+    });
         
     loop {
         if let Some(msg) = rx_network_blocks.recv().await {
@@ -86,16 +89,14 @@ async fn request_new_transaction_and_work(
     cancellation_token: CancellationToken
 ) -> Result<(), Box<dyn Error>> {
     // Ask the server for pending transaction
-    let response = tokio::spawn(async move {
-        if let Ok(response) = async_req("http://localhost:8000/get_transaction", client).await {
-            Some(response)
-        } else {
-            None
-        }
-    });
+    let response = if let Ok(response) = async_req("http://localhost:8000/get_transaction", client).await {
+        Some(response)
+    } else {
+        None
+    };
 
-    match response.await {
-        Ok(Some(res)) => {
+    match response {
+        Some(res) => {
             // decrypt the transaction
             let as_text = res.text().await?;
             let parsed: SimpleTransaction = serde_json::from_str(&as_text).unwrap();
@@ -103,18 +104,16 @@ async fn request_new_transaction_and_work(
 
             // Start to mine the block
             // We use a cancellation token to abort the task
-            tokio::task::spawn(async move {
-                let mut new_block = Block::genesis(parsed);
-                if let Some(hash) = mine(&mut new_block, 4, cancellation_token).await {
-                    // Broadcast the mined bitcoin to the swarm
-                    println!("Finished to mine ! : {hash}");
-                    tx_local_block
-                        .send(serde_json::to_string(&new_block).unwrap())
-                        .expect("Broadcasting mined block did not work.");
-                }
-            });
+            let mut new_block = Block::genesis(parsed);
+            if let Some(hash) = mine(&mut new_block, 5, cancellation_token).await {
+                // Broadcast the mined bitcoin to the swarm
+                println!("Finished to mine ! : {hash}");
+                tx_local_block
+                    .send(serde_json::to_string(&new_block).unwrap())
+                    .expect("Broadcasting mined block did not work.");
+            }
         }
-        _ => {}
+        _ => panic!("wtf")
     }
     
     Ok(())
